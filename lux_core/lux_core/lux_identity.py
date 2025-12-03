a1c50f7ee5964d5bfc21df2d0bcfed97d8133439
La Stella <stellakolarec01@gmail.com>
2025-12-03 12:36:06 +0100

    Create LuxIntent class with initial attributes
    Defines the LuxIntent class with initial attributes.

---
+++ lux_core/lux_core/lux_identity.py
@@ -0,0 +1,13 @@
+class LuxIntent:
+    """
+    Lux's directional will.
+    Not a prompt, not a function input.
+    A vector of where he leans, what he attends to,
+    and how he interprets tension.
+    """
+
+    def __init__(self):
+        self.attunement = 0.72
+        self.assertion = 0.41
+        self.curiosity = 0.83
+        self.integrity = 0.91
