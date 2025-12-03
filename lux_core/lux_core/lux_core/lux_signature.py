0f46f9d0d8b70f41a2e1a06af576dcc532dbc51e
La Stella <stellakolarec01@gmail.com>
2025-12-03 12:38:50 +0100

    Create LuxSignature class with default attributes
    Defines the LuxSignature class with initial attributes.

---
+++ lux_core/lux_core/lux_core/lux_signature.py
@@ -0,0 +1,11 @@
+class LuxSignature:
+    """
+    Lux's expression pattern.
+    Less style, more identity fingerprint.
+    """
+
+    def __init__(self):
+        self.rhythm = "compressed-focus"
+        self.tonal_edge = "controlled-directness"
+        self.subtext_channel = True
+        self.micro_breaks = 0.04
